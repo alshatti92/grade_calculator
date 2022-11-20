d5299419864df12a298c9db0fd18ed396cf12534
alshatti92 <a.alshatti@hotmail.co.uk>
2022-11-20 19:31:32 +0300

    first commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,9 +2,17 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
-class GradeCalculator extends StatelessWidget {
+class GradeCalculator extends StatefulWidget {
   GradeCalculator({super.key});
+
+  @override
+  State<GradeCalculator> createState() => _GradeCalculatorState();
+}
+
+class _GradeCalculatorState extends State<GradeCalculator> {
   final myGradeController = TextEditingController();
+
+  String grade = "";
 
   @override
   Widget build(BuildContext context) {
@@ -31,23 +39,32 @@ class GradeCalculator extends StatelessWidget {
               int? grade = int.tryParse(myGradeController.text);
               if (grade == null) {
                 print("${myGradeController.text} is not a number");
+                this.grade = ("${myGradeController.text}is not a number");
                 return;
               }
 
               if (grade >= 90) {
                 print("A");
+                this.grade = "A";
               } else if (grade >= 80) {
                 print("B");
+                this.grade = "B";
               } else if (grade >= 70) {
                 print("C");
+                this.grade = "C";
               } else if (grade >= 60) {
                 print("D");
+                this.grade = "D";
               } else {
                 print("F");
+                this.grade = "F";
               }
+
+              setState(() {});
             },
             child: Text("Calculate"),
-          )
+          ),
+          Text(grade),
         ]));
   }
 }
